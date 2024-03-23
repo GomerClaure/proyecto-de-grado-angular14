337fe402b7b833b804c0fc8648099da038427d5d
Gomer_Claure <gomerclaureb@gmail.com>
2024-03-23 14:41:33 -0400

    Agregando auth service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modulos/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
 const routes: Routes = [
   {
     path:'',
@@ -13,8 +14,10 @@ const routes: Routes = [
   {
     path:'lista',
     loadChildren:()=>import('../app/modulos/propietario/listas/listas.module').then(m=>m.ListasModule)
+  },{
+  path: 'login',
+  loadChildren: () => import('../app/auth/login/login.component').then(m => m.LoginComponent)
   }
-  
 ];
 
 @NgModule({ 
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+//import { RegistrarComponent } from '../../registro/registrar-platillo/registrar.component';
+import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/auth/session.service';
+import { FormBuilder, FormGroup, Validators,  } from '@angular/forms';
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent 
+{
+  public formulario: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private formBuilder: FormBuilder, private sessionService: SessionService) {
+    this.formulario = this.formBuilder.group({
+      user: [null,Validators.required, Validators.minLength(6), Validators.maxLength(20)],
+      password: [null,Validators.required, Validators.minLength(6), Validators.maxLength(20)]
+    });
   }
 
-}
+  public login() {
+    const { user, password } = this.formulario.value;
+    this.sessionService.login(user, password).subscribe(
+      res => {
+        if (res) {
+          this.router.navigate(['/home']);
+        }
+      },
+      err => {
+        console.log(err);
+        console.log(err.error.message);
+       alert('Usuario o contraseña incorrectos.');
+      });
+  }
+  Propietario() {
+    this.router.navigate(['/registro']);
+    }
+  }
