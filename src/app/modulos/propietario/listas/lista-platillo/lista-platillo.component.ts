e6966b22182b585516dbf83a1b63d1e301c7a009
Gomer_Claure <gomerclaureb@gmail.com>
2024-03-26 21:11:29 -0400

    Leyendo buscador

--- src/app/modulos/propietario/listas/lista-platillo/lista-platillo.component.ts
+++ src/app/modulos/propietario/listas/lista-platillo/lista-platillo.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Platillo } from 'src/app/modelos/Platillo';
 import { PlatillosService } from 'src/app/services/platillos/platillos.service';
 import { environment } from 'src/environments/environment';
+import { ModalEliminarComponent } from '../modal-eliminar/modal-eliminar.component';
 
 @Component({
   selector: 'app-lista-platillo',
@@ -14,6 +15,7 @@ export class ListaPlatilloComponent {
   filterPlatillos='';
   platillos: Platillo[] = [];
   storageUrl = environment.backendStorageUrl;
+  textoBuscador:string = '';
   constructor(private router: Router, private platilloService: PlatillosService) {
   }
 
@@ -24,6 +26,7 @@ export class ListaPlatilloComponent {
   editarPlatillo(id: number) {
     this.router.navigate(['lista/editar-platillo'], { queryParams: { platilloId: id } });
   }
+
 
   getPlatillos() {
     this.platilloService.getPlatillos().subscribe(
@@ -37,4 +40,7 @@ export class ListaPlatilloComponent {
     );
   }
 
+  onSearchChange(searchValue: string): void {  
+    console.log(searchValue);
+  }
 }
