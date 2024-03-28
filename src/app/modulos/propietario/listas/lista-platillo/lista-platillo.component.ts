ac68415f5f9ed6f4368df63ece8e13f8529c6163
Luisa Mora <luisamora895@gmail.com>
2024-03-28 10:03:36 -0400

    Add filtrar platillos por categoria

--- src/app/modulos/propietario/listas/lista-platillo/lista-platillo.component.ts
+++ src/app/modulos/propietario/listas/lista-platillo/lista-platillo.component.ts
@@ -14,6 +14,7 @@ import { ModalEliminarComponent } from '../modal-eliminar/modal-eliminar.compone
 export class ListaPlatilloComponent {
   filterPlatillos='';
   platillos: Platillo[] = [];
+  platillosFiltrados:Platillo[]=[];
   storageUrl = environment.backendStorageUrl;
   textoBuscador:string = '';
   constructor(private router: Router, private platilloService: PlatillosService) {
@@ -42,5 +43,20 @@ export class ListaPlatilloComponent {
 
   onSearchChange(searchValue: string): void {  
     console.log(searchValue);
+    this.textoBuscador = searchValue.trim().toLowerCase();
+    this.filtrarPlatillos();
+  }
+
+  filtrarPlatillos():void{
+    if (this.textoBuscador === '') {
+      // Si el campo de búsqueda está vacío, mostrar todos los platillos
+      this.platillosFiltrados = this.platillos;
+    } else {
+      // Filtrar los platillos por nombre y categoría
+      this.platillosFiltrados = this.platillos.filter(platillo =>
+        platillo.nombre.toLowerCase().includes(this.textoBuscador) || platillo.categoria.nombre.toLowerCase().includes(this.textoBuscador)
+      );
+    }
+
   }
 }
