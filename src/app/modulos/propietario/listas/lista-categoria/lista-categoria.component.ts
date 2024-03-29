82b49d753e36117bc4970b62606f2d72bb57078e
Luisa Mora <luisamora895@gmail.com>
2024-03-29 17:26:08 -0400

    add lista de categorias

--- src/app/modulos/propietario/listas/lista-categoria/lista-categoria.component.ts
+++ src/app/modulos/propietario/listas/lista-categoria/lista-categoria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/modelos/Categoria';
+import { CategoriaService } from 'src/app/services/categoriaPlatillo/categoria.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-lista-categoria',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaCategoriaComponent implements OnInit {
 
-  constructor() { }
+  categorias:Categoria[]=[];
+  storageUrl = environment.backendStorageUrl;
 
+  constructor(private categoriasService:CategoriaService) { 
+  }
   ngOnInit(): void {
+    this.getCategorias();
+  }
+
+  getCategorias() {
+    this.categoriasService.getCategorias().subscribe(
+      (data: any) => { // Ajusta el tipo de datos esperado
+        this.categorias = data.categorias; // Almacena las categorías obtenidas en la variable categorias
+        console.log(data)
+      },
+      error => {
+        console.error('Error obteniendo categorías:', error);
+      }
+    );
   }
 
 }
