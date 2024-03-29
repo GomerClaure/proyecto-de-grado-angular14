9656d730d6e5810e5538d38ca41db8e851490baa
Luisa Mora <luisamora895@gmail.com>
2024-03-29 14:00:31 -0400

    add categoria img

--- src/app/modulos/propietario/registro/registrar-categoria/registrar-categoria.component.ts
+++ src/app/modulos/propietario/registro/registrar-categoria/registrar-categoria.component.ts
@@ -7,17 +7,37 @@ import { CategoriaService } from 'src/app/services/categoriaPlatillo/categoria.s
   styleUrls: ['./registrar-categoria.component.scss']
 })
 export class RegistrarCategoriaComponent {
-
+  imageUrl: string | ArrayBuffer | null;
+  imageWidth: number = 350; 
+  imageHeight: number = 250;
   formularioCategoria:FormGroup
+  selectedFile: File = new File([''], ''); 
+
   constructor(private formBuilder:FormBuilder, private categoriaService:CategoriaService) { 
     this.formularioCategoria=this.formBuilder.group({
       nombre:[null,Validators.required]
     })
+    this.imageUrl = 'assets/image/27002.jpg';
    }
+
+     // Función para previsualizar la imagen seleccionada
+  onFileSelected(event: any) {
+    this.selectedFile = <File>event.target.files[0];
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imageUrl = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
    guardarCategoria(){
     if(this.formularioCategoria.valid){
       let nombre=this.formularioCategoria.value;
+
       console.log(nombre);
+      console.log(this.imageUrl);
       this.categoriaService.saveCategoria(nombre).subscribe(
         res=>{
           alert('Categoria Guardada');
@@ -27,6 +47,7 @@ export class RegistrarCategoriaComponent {
           console.log(err);
         }
       )
+      this.imageUrl='assets/image/27002.jpg';
     }else{
      //Formulario Invalido
     }
--- src/app/services/categoriaPlatillo/categoria.service.ts
+++ src/app/services/categoriaPlatillo/categoria.service.ts
@@ -15,10 +15,8 @@ export class CategoriaService {
 
   // Obtiene todas las categorias de platillos
   getCategorias(){
-
     return this.http.get(`${this.BASE_URL}/menu/categoria`, { headers: this.headers });
   }
-
   // Obtiene una categoria de platillos por su id
   getCategoria(id: string){
     return this.http.get(`${this.BASE_URL}/menu/categoria/` + id);
