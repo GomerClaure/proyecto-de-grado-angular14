e1a8d0b194bb27522906d076a14294a86109c603
Gomer Claure <gomerclaureb@gmail.com>
2024-03-31 22:25:56 -0400

    ADD Boceto inicial del menu generado

--- src/app/modulos/propietario/menu/generar/generar.component.ts
+++ src/app/modulos/propietario/menu/generar/generar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/modelos/Categoria';
+import { Platillo } from 'src/app/modelos/Platillo';
 
 @Component({
   selector: 'app-generar',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerarComponent implements OnInit {
 
-  constructor() { }
+  public platillos: Platillo[];
+  public categorias: Categoria[];
+  public platillosFiltrados: Platillo[];
 
-  ngOnInit(): void {
+  constructor() {
+    this.platillos = [];
+    this.categorias = [];
+    this.platillosFiltrados = [];
   }
 
+  ngOnInit(): void {
+    this.cargarPlatillos();
+  }
+  activeCategoria = 0; // Categoría activa inicialmente
+
+  cambiarCategoria(idCategoria: number) {
+    this.activeCategoria = idCategoria; // Cambia la categoría activa al hacer clic en un enlace de categoría
+    if (idCategoria === 0) {
+      this.platillosFiltrados = this.platillos;
+    }else{
+      this.platillosFiltrados = this.platillos.filter(platillo => platillo.categoria.id === idCategoria);
+    }
+  }
+
+  cargarPlatillos() {
+    const categorias: Categoria[] = [
+      { id: 1, nombre: "Comida rápida", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
+      { id: 2, nombre: "Desayunos", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
+      { id: 3, nombre: "Almuerzos", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
+      { id: 4, nombre: "Cenas", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
+      { id: 5, nombre: "Postres", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
+      { id: 6, nombre: "Bebidas", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
+      { id: 7, nombre: "Snacks", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
+    ];
+    const imagenes = ['http://192.168.0.30:8000/storage/platillos/b7a44a65420f3efeb010b040e43ece42.webp',
+     'http://192.168.0.30:8000/storage/platillos/125bfeb29dc08066308218a092f664fb.jpg',
+  ];
+      
+    this.categorias = categorias;
+    for (let i = 1; i <= 30; i++) {
+      const categoriaIndex = Math.floor(Math.random() * categorias.length);
+      const categoria = categorias[categoriaIndex];
+      this.platillos.push({
+        id: i,
+        nombre: `Platillo ${i}`,
+        descripcion: `Descripción del platillo ${i}`,
+        precio: Math.floor(Math.random() * 100) + 50,
+        imagen: Math.random() > 0.5 ? imagenes[0] : imagenes[1],
+        id_menu: 1,
+        categoria: { ...categoria }
+      });
+    }
+    this.platillosFiltrados = this.platillos;
+  }
 }
