3cea42a34fd94233798040e500dad1db66a55d1b
Gomer_Claure <gomerclaureb@gmail.com>
2024-04-02 01:25:16 -0400

    ADD conexion con backend

--- src/app/modulos/propietario/menu/generar/generar.component.ts
+++ src/app/modulos/propietario/menu/generar/generar.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Categoria } from 'src/app/modelos/Categoria';
 import { Platillo } from 'src/app/modelos/Platillo';
+import { MenuService } from 'src/app/services/menu/menu.service';
+import { CategoriaService } from 'src/app/services/categoriaPlatillo/categoria.service';
+import { Menu } from 'src/app/modelos/Menu';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-generar',
@@ -9,22 +13,27 @@ import { Platillo } from 'src/app/modelos/Platillo';
 })
 export class GenerarComponent implements OnInit {
 
-  public platillos: any[];
+  public platillos: Platillo[];
   public categorias: Categoria[];
-  public platillosFiltrados: any[];
+  public platillosFiltrados: Platillo[];
+  public menu!: Menu;
   public imgPortada: string;
   public selectedFile: File;
+  public tema: string;
+  public baseUrl = environment.backendStorageUrl;
 
-  constructor() {
+  constructor(private menuService: MenuService, private categoriaService: CategoriaService) {
     this.platillos = [];
     this.categorias = [];
     this.platillosFiltrados = [];
     this.selectedFile = new File([''], '');
     this.imgPortada = 'assets/image/27002.jpg';
+    this.tema = 'light-theme';
     // this.themeSwitch = new ElementRef(null);
   }
 
   ngOnInit(): void {
+    this.cargarCategorias();
     this.cargarPlatillos();
   }
   activeCategoria = 0; // Categoría activa inicialmente
@@ -39,36 +48,39 @@ export class GenerarComponent implements OnInit {
   }
 
   cargarPlatillos() {
-    const categorias: Categoria[] = [
-      { id: 1, nombre: "Comida rápida", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
-      { id: 2, nombre: "Desayunos", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
-      { id: 3, nombre: "Almuerzos", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
-      { id: 4, nombre: "Cenas", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
-      { id: 5, nombre: "Postres", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
-      { id: 6, nombre: "Bebidas", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
-      { id: 7, nombre: "Snacks", imagen: "http://localhost:8000/storage/categorias/15cf2210972a9fb8c61f9a6c557e189c.jpg" },
-    ];
-    const imagenes = ['http://localhost:8000/storage/platillos/85d976ac0070153290060f9bd67b304b.jpg',
-      'http://localhost:8000/storage/platillos/b4616ff52c8e0eef2d04f07048d40167.jpg',
-    ];
-
-    this.categorias = categorias;
-    for (let i = 1; i <= 30; i++) {
-      const categoriaIndex = Math.floor(Math.random() * categorias.length);
-      const categoria = categorias[categoriaIndex];
-      this.platillos.push({
-        id: i,
-        nombre: `Platillo ${i}`,
-        descripcion: `Descripción del platillo ${i}`,
-        precio: Math.floor(Math.random() * 100) + 50,
-        imagen: Math.random() > 0.5 ? imagenes[0] : imagenes[1],
-        id_menu: 1,
-        categoria: { ...categoria },
-        plato_disponible: true,
-      });
-    }
-    this.platillosFiltrados = this.platillos;
+    this.menuService.getMenu().subscribe(
+      res => {
+        console.log(res.platillos);
+        this.menu = res.menu;
+        this.tema = this.menu.tema;
+        let checkSwitch = document.getElementById('themeSwitch');
+        if (checkSwitch && this.tema === 'dark-theme') checkSwitch.click();
+        let collapse = document.getElementById('collapseImgPortada');
+        if (collapse && this.menu.portada!='') 
+          collapse.classList.add('show');
+          this.imgPortada = this.baseUrl+this.menu.portada;
+        this.platillos = res.platillos;
+        this.platillosFiltrados = this.platillos;
+        
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
+
+  cargarCategorias() {
+    console.log('Cargando categorías');
+    this.categoriaService.getCategorias().subscribe(
+      (res: any) => {
+        this.categorias = res.categorias;
+        console.log('Categorías cargadas---');
+      },
+      err => {
+        console.log(err);
+      }
+    );
+    }
 
   onFileSelected(event: any) {
     this.selectedFile = <File>event.target.files[0];
@@ -77,7 +89,6 @@ export class GenerarComponent implements OnInit {
     if (file) {
       const reader = new FileReader();
       reader.onload = (e: any) => {
-        //set imgPortada with the base64 string
         this.imgPortada = e.target.result;
         let collapse = document.getElementById('collapseImgPortada');
 
@@ -96,8 +107,10 @@ export class GenerarComponent implements OnInit {
     console.log(switchValue);
     let body = document.getElementById('body-generar-menu');
     if (switchValue) {
-      body?.classList.add('dark-theme');
+      this.tema = 'dark-theme';
+      body?.classList.add(this.tema);
     } else {
+      this.tema = 'light-theme';
       // Cambiar a tema claro
       body?.classList.remove('dark-theme');
     }
@@ -109,11 +122,29 @@ export class GenerarComponent implements OnInit {
       const platillo = this.platillos[index]; // Obtiene el platillo usando el índice
       platillo.plato_disponible = false;
     }
-    
+
     }
 
     guardarMenu() {
+      const formData = new FormData();
+      console.log(this.selectedFile);
+      if (this.selectedFile.size > 0)
+        console.log('Imagen seleccionada');
+        formData.append('portada', this.selectedFile);
+      formData.append('tema', this.tema);
+      formData.append('platillos', JSON.stringify(this.platillos));
+      formData.append('id_menu', this.menu.id.toString());
       console.log('Guardando menú');
+      this.menuService.saveMenu(formData).subscribe(
+        res => {
+          console.log(res);
+          alert('Menú guardado');
+        },
+        err => {
+          console.log(err);
+        }
+      );
+      
     }
 
   }
