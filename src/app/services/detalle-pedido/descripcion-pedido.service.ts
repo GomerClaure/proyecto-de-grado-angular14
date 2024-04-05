0afe6b38125bc7e764e6a89e94f9290de3882e2d
Luisa Mora <luisamora895@gmail.com>
2024-04-04 21:00:57 -0400

    Add guardar platillo y la descripcion segun id

--- src/app/services/detalle-pedido/descripcion-pedido.service.ts
+++ src/app/services/detalle-pedido/descripcion-pedido.service.ts
@@ -10,16 +10,18 @@ export class DescripcionPedidoService {
   platilloNombreSeleccionado(id:number){
     this.index=id;
   }
-  addDescripcion(descripcion:string){
-        // Asegúrate de que index tenga un valor válido
-        if (this.index >= 0 && this.index < this.descripciones.length) {
-          // Agrega la descripción y el ID al arreglo descripciones
-          this.descripciones.push({ id: this.index, descripcion: descripcion });
-        }
-        console.log(descripcion)
-        console.log(this.index)
-  }  
+  addDescripcion(descripcion: string) {
+    // Asegúrate de que index tenga un valor válido
+    if (this.index >= 0) {
+      // Agrega la descripción y el ID al arreglo descripciones
+      this.descripciones.push({ id: this.index, descripcion: descripcion });
+    }
+    console.log(descripcion);
+    console.log(this.index);
+    // Restablecer el índice después de agregar la descripción
+    this.index = 0;
+  }
   getDescripciones(){
-    return this.descripciones;
+    return this.descripciones; 
   }
 }
