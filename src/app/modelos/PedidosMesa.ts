9246db6bba72984fe86552c456454d9e4a715231
Luisa Mora <luisamora895@gmail.com>
2024-05-23 18:46:31 -0400

    Add listar las mesas con pedidos

---
+++ src/app/modelos/PedidosMesa.ts
@@ -0,0 +1,5 @@
+export interface PedidosMesa {
+    nombreMesa: string;
+    pedidos: any[]; // Aquí puedes definir el tipo específico de los pedidos si lo deseas
+  }
+  
--- src/app/modulos/mesero/lista-pedidos/lista-pedidos.component.ts
+++ src/app/modulos/mesero/lista-pedidos/lista-pedidos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PedidoService } from 'src/app/services/pedido/pedido.service';
+import { PedidosMesa } from 'src/app/modelos/PedidosMesa';
 
 @Component({
   selector: 'app-lista-pedidos',
@@ -9,24 +10,39 @@ import { PedidoService } from 'src/app/services/pedido/pedido.service';
 export class ListaPedidosComponent implements OnInit {
   pedidos: any[] = [];
   errorMessage: string = '';
+  pedidosPorMesa: PedidosMesa[] = []; // Utiliza la interfaz PedidosMesa como tipo para el array
 
-  constructor(private pedidoService:PedidoService) { }
+  constructor(private pedidoService: PedidoService) { }
 
   ngOnInit(): void {
     this.obtenerPedidos();
-
   }
    
   obtenerPedidos(): void {
     this.pedidoService.getPedidos().subscribe(
       (response) => {
         this.pedidos = response.pedidos;
-        console.log(this.pedidos)
+        this.agruparPedidosPorMesa();
       },
       (error) => {
         this.errorMessage = 'Error al obtener los pedidos';
         console.error(error);
       }
     );
+  }
+
+  agruparPedidosPorMesa(): void {
+    this.pedidos.forEach(pedido => {
+      const numeroMesa = pedido.cuenta.id_mesa;
+      const nombreMesa = pedido.cuenta.mesa.nombre; // Obtén el nombre de la mesa
+      const pedidosMesa = this.pedidosPorMesa.find(item => item.nombreMesa === nombreMesa); // Busca si ya existe la mesa en el array
+
+      if (!pedidosMesa) { // Si la mesa no existe, la agregamos al array con sus pedidos
+        this.pedidosPorMesa.push({ nombreMesa: nombreMesa, pedidos: [pedido] });
+      } else { // Si la mesa ya existe, agregamos el pedido al array de pedidos de esa mesa
+        pedidosMesa.pedidos.push(pedido);
+      }
+    });
+    console.log(this.pedidosPorMesa);
   }
 }
