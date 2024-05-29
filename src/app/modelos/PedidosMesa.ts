72e082c9fbd1621b9bddf7dd5cd1affa9a2dfbba
Luisa Mora <luisamora895@gmail.com>
2024-05-29 16:45:48 -0400

    Mostrando pedidos con el total del precio por pedidos y total de platos parciales

--- src/app/modelos/PedidosMesa.ts
+++ src/app/modelos/PedidosMesa.ts
@@ -2,4 +2,9 @@ export interface PedidosMesa {
     nombreMesa: string;
     pedidos: any[]; // Aquí puedes definir el tipo específico de los pedidos si lo deseas
   }
+export interface PedidosParaMostrarMesa{
+  cantidad:number;
+  nomplatillo:string;
+  totalPedido:number
+}
   
--- src/app/modulos/mesero/modal-pedidos/modal-pedidos.component.ts
+++ src/app/modulos/mesero/modal-pedidos/modal-pedidos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PedidosParaMostrarMesa } from 'src/app/modelos/PedidosMesa';
 import { PedidosDeMesaService } from 'src/app/services/pedido/pedidos-de-mesa.service';
 
 @Component({
@@ -9,18 +10,48 @@ import { PedidosDeMesaService } from 'src/app/services/pedido/pedidos-de-mesa.se
 export class ModalPedidosComponent implements OnInit {
   pedidosDeMesa: any[] = [];
   nombreMesa:string='';
-
+  PedidosParaMostrar: { pedidoId: number, platos: PedidosParaMostrarMesa[], totalPedido: number }[] = [];
   constructor(private pedidoServiceMesa: PedidosDeMesaService) { }
 
   ngOnInit(): void {
     this.pedidoServiceMesa.pedidosMesa$.subscribe(data => {
       this.pedidosDeMesa = data.pedidos;
       this.nombreMesa = data.nombreMesa;
-      console.log("Datos cargados:", this.pedidosDeMesa, "Mesa:", this.nombreMesa);
-      this.Ordenar();
+      this.ordenar();
     });
   }
-  Ordenar(){
-    
+  ordenar() {
+    this.PedidosParaMostrar = this.pedidosDeMesa.map(pedido => {
+      const cantidadesPorPlatillo: { [nombrePlatillo: string]: PedidosParaMostrarMesa } = {};
+      let totalPedido = 0;
+
+      pedido.platos.forEach((plato: any) => {
+        const nombrePlatillo = plato.nombre;
+        const cantidad = plato.pivot.cantidad;
+        const precio = plato.precio;
+
+        if (cantidadesPorPlatillo.hasOwnProperty(nombrePlatillo)) {
+          cantidadesPorPlatillo[nombrePlatillo].cantidad += cantidad;
+          cantidadesPorPlatillo[nombrePlatillo].totalPedido += cantidad * precio;
+        } else {
+          cantidadesPorPlatillo[nombrePlatillo] = {
+            cantidad: cantidad,
+            nomplatillo: nombrePlatillo,
+            totalPedido: cantidad * precio
+          };
+        }
+
+        totalPedido += cantidad * precio;
+      });
+
+      return {
+        pedidoId: pedido.id,
+        platos: Object.values(cantidadesPorPlatillo),
+        totalPedido: totalPedido
+      };
+    });
+
+    console.log(this.PedidosParaMostrar);
   }
+
 }
