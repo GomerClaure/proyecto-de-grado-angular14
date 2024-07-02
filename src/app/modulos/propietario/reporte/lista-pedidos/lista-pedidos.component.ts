7cb662565439263624b1fc1d056c853328d4d48d
Gomer_Claure <gomerclaureb@gmail.com>
2024-07-02 15:29:21 -0400

    ADD ejemplo de grafico ng-chart

--- src/app/modulos/propietario/reporte/lista-pedidos/lista-pedidos.component.ts
+++ src/app/modulos/propietario/reporte/lista-pedidos/lista-pedidos.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ReporteService } from 'src/app/services/reporte/reporte.service';
 import { Reporte } from 'src/app/modelos/Reporte';
+import { ChartConfiguration, ChartData, ChartEvent } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-lista-pedidos',
@@ -8,7 +10,66 @@ import { Reporte } from 'src/app/modelos/Reporte';
   styleUrls: ['./lista-pedidos.component.scss']
 })
 export class ListaPedidosComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective<'bar'> | undefined;
 
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+    },
+  };
+  public barChartType = 'bar' as const;
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    ],
+  };
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40,
+    ];
+
+    this.chart?.update();
+  }
   public fechaInicio: string;// hace siete dias
   public fechaFin: string;// hoy a las 11:59:59
   public reporte: Reporte;
--- src/app/modulos/propietario/reporte/reporte.module.ts
+++ src/app/modulos/propietario/reporte/reporte.module.ts
@@ -5,8 +5,11 @@ import { ListaPedidosComponent } from './lista-pedidos/lista-pedidos.component';
 import { ListaVentasComponent } from './lista-ventas/lista-ventas.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { FormsModule } from '@angular/forms';
-
-
+// import { ChartConfiguration, ChartData, ChartEvent } from 'chart.js';
+// import { BaseChartDirective } from 'ng2-charts';
+// import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     ReporteRoutingModule,
     ComponentsModule,
-    FormsModule
+    FormsModule,
+    NgChartsModule,
   ]
 })
 export class ReporteModule { }
