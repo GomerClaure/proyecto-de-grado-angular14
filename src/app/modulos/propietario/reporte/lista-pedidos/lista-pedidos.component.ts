9de16af1caeef3113d54337a55613b640f5d7e38
Gomer_Claure <gomerclaureb@gmail.com>
2024-07-03 21:22:16 -0400

    ADD linechart y cambio de colores a los primeros graficos

--- src/app/modulos/propietario/reporte/lista-pedidos/lista-pedidos.component.ts
+++ src/app/modulos/propietario/reporte/lista-pedidos/lista-pedidos.component.ts
@@ -11,6 +11,7 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class ListaPedidosComponent implements OnInit {
   @ViewChild(BaseChartDirective) barChart: BaseChartDirective<'bar'> | undefined;
+  @ViewChild('lineChartMonto') lineChartMonto: BaseChartDirective<'line'> | undefined;
   @ViewChild('pieChart') pieChart: BaseChartDirective<'pie'> | undefined;
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
@@ -26,20 +27,65 @@ export class ListaPedidosComponent implements OnInit {
       },
     },
   };
+  
+  public lineChartOptions: ChartConfiguration<'line'>['options'] = {
+    elements: {
+      line: {
+        tension: 0.5,
+      },
+    },
+    scales: {
+      y: {
+        position: 'left',
+      },
+      y1: {
+        position: 'right',
+        grid: {
+          display: false, // Deshabilita la rejilla
+        },
+        ticks: {
+          display: false, // Deshabilita los ticks
+        },
+      },
+    },
+    plugins: {
+      legend: { display: false },
+    },
+  };
 
-  public barChartDataMonto: ChartData<'bar'> = {
+  public lineChartDataMonto: ChartData<'line'> = {
     labels: [],
     datasets: [
-      { data: [], label: 'Monto total de pedidos' },
+      {
+        data: [],
+        label: 'Monto total de pedidos',
+        backgroundColor: 'rgba(212, 167, 66, 0.2)', // Color de fondo
+        borderColor: '#d6a742', // Color de línea más oscuro
+        pointBackgroundColor: '#d6a742', // Color de punto
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(212, 167, 66, 0.8)', // Color de punto al pasar el mouse
+        fill: 'origin',
+      },
     ],
   };
+  
 
   public barChartDataCantidad: ChartData<'bar'> = {
     labels: [],
     datasets: [
-      { data: [], label: 'Cantidad de pedidos' },
+      {
+        data: [],
+        label: 'Cantidad de pedidos',
+        backgroundColor: '#70c1b3', // Color de barra (verdoso)
+        borderColor: 'rgba(112, 193, 179, 1)', // Color de borde de barra
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(112, 193, 179, 0.4)', // Color de fondo al pasar el mouse
+        hoverBorderColor: 'rgba(112, 193, 179, 1)', // Color de borde al pasar el mouse
+      },
     ],
   };
+  
 
   public pieChartOptions: ChartConfiguration<'pie'>['options'] = {
     plugins: {
@@ -58,6 +104,7 @@ export class ListaPedidosComponent implements OnInit {
 
   public barChartType = 'bar' as const;
   public pieChartType = 'pie' as const;
+  public lineChartType = 'line' as const;
 
   public fechaInicio: string; // hace siete dias
   public fechaFin: string; // hoy a las 11:59:59
@@ -90,8 +137,10 @@ export class ListaPedidosComponent implements OnInit {
     formData.append('id_restaurante', idRestaurante || '');
 
     this.reporteService.getReportePedidos(formData).subscribe((reporte: Reporte) => {
-      this.barChartDataMonto.labels = reporte.montoTotalPedidosPorDia.map((item) => item.fecha);
-      this.barChartDataMonto.datasets[0].data = reporte.montoTotalPedidosPorDia.map((item) => item.monto);
+      // this.barChartDataMonto.labels = reporte.montoTotalPedidosPorDia.map((item) => item.fecha);
+      // this.barChartDataMonto.datasets[0].data = reporte.montoTotalPedidosPorDia.map((item) => item.monto);
+      this.lineChartDataMonto.labels = reporte.montoTotalPedidosPorDia.map((item) => item.fecha);
+      this.lineChartDataMonto.datasets[0].data = reporte.montoTotalPedidosPorDia.map((item) => item.monto);
 
       this.barChartDataCantidad.labels = reporte.cantidadPedidosPorDia.map((item) => item.fecha);
       this.barChartDataCantidad.datasets[0].data = reporte.cantidadPedidosPorDia.map((item) => item.cantidad);
@@ -101,6 +150,7 @@ export class ListaPedidosComponent implements OnInit {
 
       this.barChart?.update();
       this.pieChart?.update();
+      this.lineChartMonto?.update();
       this.reporte = reporte;
     });
   }
@@ -113,9 +163,9 @@ export class ListaPedidosComponent implements OnInit {
     console.log(event, active);
   }
 
-  public randomize(): void {
-    this.barChartDataMonto.datasets[0].data = this.barChartDataMonto.datasets[0].data.map(() => Math.round(Math.random() * 10000));
-    this.barChartDataCantidad.datasets[0].data = this.barChartDataCantidad.datasets[0].data.map(() => Math.round(Math.random() * 10));
-    this.barChart?.update();
-  }
+  // public randomize(): void {
+  //   this.barChartDataMonto.datasets[0].data = this.barChartDataMonto.datasets[0].data.map(() => Math.round(Math.random() * 10000));
+  //   this.barChartDataCantidad.datasets[0].data = this.barChartDataCantidad.datasets[0].data.map(() => Math.round(Math.random() * 10));
+  //   this.barChart?.update();
+  // }
 }
