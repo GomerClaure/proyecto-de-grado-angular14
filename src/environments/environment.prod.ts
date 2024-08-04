e21e7a40bc410bf4e5d028c7bb8f2a13c8af55ee
Gomer_Claure <gomerclaureb@gmail.com>
2024-08-04 01:12:44 -0400

    FIX Categorias y modo produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,16 @@
 export const environment = {
-  production: true,
+  production: false,
+  // backendUrl: 'http://localhost:8000/api',
+  // backendStorageUrl: 'http://localhost:8000',
+  // frontDominio: 'http://localhost:4200'
+
   backendUrl: ' http://192.168.0.17:8000/api',
   backendStorageUrl: ' http://192.168.0.17:8000',
-  frontDominio: ' http://192.168.0.17:8000:4200',
+  frontDominio: ' http://192.168.0.17:4200',
   websocketConfig:{
     key: '1234',
     cluster: 'mt1',
-    wsHost: 'localhost',
+    wsHost: '192.168.0.17',
     wsPort: 6001,
     forceTLS: false,
     enabledTransports: ['ws'],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,9 @@
 
 export const environment = {
   production: false,
-  // backendUrl: 'http://localhost:8000/api',
-  // backendStorageUrl: 'http://localhost:8000',
-  // frontDominio: 'http://localhost:4200'
-
-  backendUrl: ' http://192.168.0.17:8000/api',
-  backendStorageUrl: ' http://192.168.0.17:8000',
-  frontDominio: ' http://192.168.0.17:8000:4200',
+  backendUrl: 'http://localhost:8000/api',
+  backendStorageUrl: 'http://localhost:8000',
+  frontDominio: 'http://localhost:4200',
   websocketConfig:{
     key: '1234',
     cluster: 'mt1',
