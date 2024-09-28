ece44ddce62db1b3626108865b6ac1cfeef98a55
Luisa Mora <luisamora895@gmail.com>
2024-09-28 17:12:15 -0400

    Cambio de maqueta

--- src/app/modulos/propietario/registro/registrar-empleado/registrar-empleado.component.ts
+++ src/app/modulos/propietario/registro/registrar-empleado/registrar-empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-empleado',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarEmpleadoComponent implements OnInit {
 
-  constructor() { }
+  formularioEmpleado: FormGroup;
+  selectedPuestos: string[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.formularioEmpleado = this.formBuilder.group({
+      nombre: [null,Validators.required],
+      apellidoPaterno: [null,Validators.required],
+      apellidoMaterno: [null,Validators.required],
+      fechaNacimiento:[null,Validators.required],
+      telefono:[null,Validators.required],
+      carnetIdentidad:[null,Validators.required],
+      direccion:[null,Validators.required],
+      correoElectronico:[null,Validators.required],
+      puesto: ['', Validators.required],
+      fechaContratacion:[null,Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    if(this.formularioEmpleado.valid){
+        const datosEmpleado=this.formularioEmpleado.value;
+        console.log(datosEmpleado)
+    }
+  }
+  onCheckboxChange(event: any) {
+    const value = event.target.value;
+    if (event.target.checked) {
+      this.selectedPuestos.push(value);
+    } else {
+      const index = this.selectedPuestos.indexOf(value);
+      if (index > -1) {
+        this.selectedPuestos.splice(index, 1);
+      }
+    }}
 }
