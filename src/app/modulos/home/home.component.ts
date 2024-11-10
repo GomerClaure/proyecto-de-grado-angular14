a3cfdb3762d9f22e3e8f7baefc90b4f528e9a96e
Gomer_Claure <gomerclaureb@gmail.com>
2024-11-10 10:48:19 -0400

    ADD animacion en pantalla home

--- src/app/modulos/home/home.component.ts
+++ src/app/modulos/home/home.component.ts
@@ -6,11 +6,14 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  restaurantName: string = '';
+  restaurantName: string = 'Bienvenido a Lugo';
+  restauranNameTemp: string = '';
   tiporestaurante: string = '';
   color: string = '';
   imageUrl: string = 'assets/image/img-home/Default.png';
   maxIntentos: number = 5;
+  animationClass: string = '';
+  titleAnimationClass: string = '';
 
   constructor(private cdr: ChangeDetectorRef) { }
 
@@ -18,52 +21,62 @@ export class HomeComponent implements OnInit {
     this.intentarRecuperar(0);
   }
   intentarRecuperar(intentosRealizados: number) {
-    this.restaurantName = sessionStorage.getItem('nombre_restaurante') || 'Restaurante';
+    this.restauranNameTemp = sessionStorage.getItem('nombre_restaurante') || 'Bienvenido a Lugo';
     this.tiporestaurante = sessionStorage.getItem('tipo_establecimiento') || '';
 
-    if ((this.restaurantName === 'Restaurante' || this.tiporestaurante === '') && intentosRealizados < this.maxIntentos) {
+    if ((this.restauranNameTemp === 'Bienvenido a Lugo' || this.tiporestaurante === '') && intentosRealizados < this.maxIntentos) {
       setTimeout(() => {
         this.intentarRecuperar(intentosRealizados + 1);
       }, 1000);
     } else {
       this.setColorAndImage();
-      this.cdr.detectChanges();
+      setTimeout(() => {
+        this.titleAnimationClass = 'zoom-in';
+      }, 100); // Añade un pequeño retraso
     }
   }
   setColorAndImage(): void {
-    switch (this.tiporestaurante.toLowerCase()) {
-      case 'restaurante':
-        this.color = '#58d68d';
-        this.imageUrl = 'assets/image/img-home/Restaurante.png';
-        break;
-      case 'cafeteria':
-        this.color = '#6e2c00';
-        this.imageUrl = 'assets/image/img-home/Cafeteria.png.png';
-        break;
-      case 'bar':
-        this.color = '#f1948a';
-        this.imageUrl = 'assets/image/img-home/Bar.png';
-        break;
-      case 'pasteleria':
-        this.color = '#a569bd';
-        this.imageUrl = 'assets/image/img-home/Pasteleria.png';
-        break;
-      case 'pizzeria':
-        this.color = '#c0392b';
-        this.imageUrl = 'assets/image/img-home/Pizzeria.png';
-        break;
-      case 'comida rapida':
-        this.color = '#d4ac0d';
-        this.imageUrl = 'assets/image/img-home/Comida-rapida.png';
-        break;
-      case 'heladeria':
-        this.color = '#117a65';
-        this.imageUrl = 'assets/image/img-home/Heladeria.png';
-        break;
-      default:
-        this.color = '#2e4053';
-        this.imageUrl = 'assets/image/img-home/Default.png';
-        break;
-    }
+    this.animationClass = 'zoom-out';
+    setTimeout(() => {
+      switch (this.tiporestaurante.toLowerCase()) {
+        case 'restaurante':
+          this.color = '#58d68d';
+          this.imageUrl = 'assets/image/img-home/Restaurante.png';
+          break;
+        case 'cafeteria':
+          this.color = '#6e2c00';
+          this.imageUrl = 'assets/image/img-home/Cafeteria.png.png';
+          break;
+        case 'bar':
+          this.color = '#f1948a';
+          this.imageUrl = 'assets/image/img-home/Bar.png';
+          break;
+        case 'pasteleria':
+          this.color = '#a569bd';
+          this.imageUrl = 'assets/image/img-home/Pasteleria.png';
+          break;
+        case 'pizzeria':
+          this.color = '#c0392b';
+          this.imageUrl = 'assets/image/img-home/Pizzeria.png';
+          break;
+        case 'comida rapida':
+          this.color = '#d4ac0d';
+          this.imageUrl = 'assets/image/img-home/Comida-rapida.png';
+          break;
+        case 'heladeria':
+          this.color = '#117a65';
+          this.imageUrl = 'assets/image/img-home/Heladeria.png';
+          break;
+        default:
+          this.color = '#2e4053';
+          this.imageUrl = 'assets/image/img-home/Default.png';
+          break;
+
+      }
+      this.restaurantName = this.restauranNameTemp;
+      this.animationClass = 'zoom-in';
+      this.cdr.detectChanges();
+    }, 800);
   }
+
 }
