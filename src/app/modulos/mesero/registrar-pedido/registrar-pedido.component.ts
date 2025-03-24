96020021b2e62c75e36df0e6a6c066aa75e5bb5f
Luisa Mora <luisamora895@gmail.com>
2025-03-24 11:15:09 -0400

    Cambios para guardar los datos

--- src/app/modulos/mesero/registrar-pedido/registrar-pedido.component.ts
+++ src/app/modulos/mesero/registrar-pedido/registrar-pedido.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PlatillosService } from 'src/app/services/platillos/platillos.service';
 import { CategoriaService } from 'src/app/services/categoriaPlatillo/categoria.service';
 import { Platillo } from 'src/app/modelos/Platillo';
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 import { DescripcionPedidoService } from 'src/app/services/detalle-pedido/descripcion-pedido.service';
 import { SessionService } from 'src/app/services/auth/session.service';
 import { ToastrService } from 'ngx-toastr';
-import { MesaService } from './../../../services/mesas/mesa.service'  ;
+import { MesaService } from './../../../services/mesas/mesa.service';
 import { Mesa } from 'src/app/modelos/Mesa'; 
 
 @Component({
@@ -19,14 +19,12 @@ import { Mesa } from 'src/app/modelos/Mesa';
   styleUrls: ['./registrar-pedido.component.scss']
 })
 export class RegistrarPedidoComponent implements OnInit {
+  mostrarModal: boolean = false; // Ahora se maneja internamente
   categoriaSeleccionada: string = '';
   idCategoriaSeleccionada: number | undefined;
   busquedaNombre: string = '';
-  //Array donde estan los platillos seleccionados
   platillosAGuardar: Platillo[] = [];
-  //Array de descripciones 
   platillosDescripciones: { id: number, descripcion: string }[] = [];
-  //Array de cantidades
   diccionarioDeCantidades: { [id: number]: number } = {};
   numeroMesa: string = '';
   platillos: Platillo[] = [];
@@ -39,8 +37,8 @@ export class RegistrarPedidoComponent implements OnInit {
   textoBuscador: string = '';
   platillosFiltrados: Platillo[] = [];
   id_restaurante: any;
-  nombreMesa:string='';
-  user:any='';
+  nombreMesa: string = '';
+  rol_user: any = '';
   mesa: Mesa | undefined;
   
   constructor(private descripcionPedidoService: DescripcionPedidoService,
@@ -50,17 +48,15 @@ export class RegistrarPedidoComponent implements OnInit {
     private categoriaService: CategoriaService,
     private sessionService: SessionService,
     private toastr: ToastrService,
-    private mesaService: MesaService 
-  ) { }
-
+    private mesaService: MesaService) { }
 
   ngOnInit(): void {
     this.id_restaurante = parseInt(sessionStorage.getItem('id_restaurante') || '0');
     this.getPlatillos();
     this.getCategorias();
     this.route.queryParams.subscribe(params => {
-      this.user = parseInt(sessionStorage.getItem('rol_empleado') || '0');
-      if (this.user === 2) {
+      this.rol_user = parseInt(sessionStorage.getItem('rol_empleado') || '0');
+      if (this.rol_user === 2) {
         this.getPrimerMesa();
       } else {
         this.numeroMesa = params['mesaSeleccionada'];
@@ -70,10 +66,8 @@ export class RegistrarPedidoComponent implements OnInit {
   }
 
   getPrimerMesa(): void {
-    this.mesaService.getMesas(this.id_restaurante.toString()).subscribe((res:any) => {
-      console.log(res.mesas[0]);
+    this.mesaService.getMesas(this.id_restaurante.toString()).subscribe((res: any) => {
       if (res && res.mesas[0] && res.mesas.length > 0) {
-        // Asignar la primera mesa
         this.numeroMesa = res.mesas[0].id.toString();
         this.nombreMesa = res.mesas[0].nombre;
       } else {
@@ -82,43 +76,34 @@ export class RegistrarPedidoComponent implements OnInit {
       }
     });
   }
-  
+
   switchStateChanged() {
-    if (this.switchState) {
-      this.tipo = 'Llevar'
-      console.log(this.tipo);
-    } else {
-      this.tipo = 'Local'
-      console.log(this.tipo)
-    }
+    this.tipo = this.switchState ? 'Llevar' : 'Local';
   }
+
   onSearchChange(searchValue: string): void {
-    console.log(searchValue);
     this.textoBuscador = searchValue.trim().toLowerCase();
     this.filtrarPlatillos();
   }
+
   filtrarPlatillos(): void {
     if (this.textoBuscador === '') {
-      // Si el campo de búsqueda está vacío, mostrar todos los platillos
       this.platillosFiltrados = this.platillos;
     } else {
-      // Filtrar los platillos por nombre y categoría
       this.platillosFiltrados = this.platillos.filter(platillo =>
         platillo.nombre.toLowerCase().includes(this.textoBuscador) || platillo.categoria.nombre.toLowerCase().includes(this.textoBuscador)
       );
     }
   }
+
   getPlatillos() {
-    let idPedidos = sessionStorage.getItem('id_restaurante')||'0';
+    let idPedidos = sessionStorage.getItem('id_restaurante') || '0';
     this.platilloService.getPlatillosMenu(idPedidos).subscribe(
       (res: any) => {
-        // Filtrar solo los platillos de la categoría seleccionada y que coincidan con el término de búsqueda
         let filteredPlatillos = res.platillo.filter((platillo: any) => {
-          if (this.idCategoriaSeleccionada && this.idCategoriaSeleccionada !== 0) {
-            return platillo.id_categoria === this.idCategoriaSeleccionada && platillo.disponible && platillo.nombre.toLowerCase().includes(this.busquedaNombre.toLowerCase());
-          } else {
-            return platillo.disponible && platillo.nombre.toLowerCase().includes(this.busquedaNombre.toLowerCase());
-          }
+          return (this.idCategoriaSeleccionada && this.idCategoriaSeleccionada !== 0) ?
+            platillo.id_categoria === this.idCategoriaSeleccionada && platillo.disponible && platillo.nombre.toLowerCase().includes(this.busquedaNombre.toLowerCase()) :
+            platillo.disponible && platillo.nombre.toLowerCase().includes(this.busquedaNombre.toLowerCase());
         });
         this.platillos = filteredPlatillos;
         this.platillosFiltrados = this.platillos;
@@ -126,18 +111,17 @@ export class RegistrarPedidoComponent implements OnInit {
       err => {
         console.log(err);
       }
-
     );
   }
+
   onChangeCategoria(event: any) {
-    this.idCategoriaSeleccionada = parseInt(event.target.value); // Convierte el valor a un número entero
-    this.getPlatillos(); // Llama a getPlatillos() para actualizar la lista de platillos según la categoría seleccionada
+    this.idCategoriaSeleccionada = parseInt(event.target.value);
+    this.getPlatillos();
   }
 
   getCategorias() {
     this.categoriaService.getCategorias(this.id_restaurante).subscribe(
       (data: any) => {
-        // Añade la opción "Todos" al principio de la lista de categorías
         this.categorias = [{ id: 0, nombre: 'Todos' }, ...data.categorias];
       },
       error => {
@@ -145,94 +129,95 @@ export class RegistrarPedidoComponent implements OnInit {
       }
     );
   }
+
   agregarPlatillo(platillo: Platillo) {
     this.pedidoselectService.agregarSeleccion(platillo);
-
   }
+
   setPlatilloSeleccionado(index: number) {
     this.descripcionPedidoService.platilloNombreSeleccionado(index);
   }
+
   retirarPlatillo(index: number) {
     this.pedidoselectService.platillosSeleccionados.splice(index, 1);
   }
+
   guardarPedido() {
     this.platillosAGuardar = this.pedidoselectService.getPlatillosSeleccionados();
     this.platillosDescripciones = this.descripcionPedidoService.getDescripciones();
 
     if (this.platillosAGuardar.length === 0) {
       this.toastr.info('No se seleccionó ningún platillo', 'Información');
-      return; // Salir del método si no hay platillos seleccionados
-  }
+      return;
+    }
 
     const platillosConDescripciones: PlatilloPedido[] = [];
     const platillosSinDescripcion: Map<number, PlatilloPedido> = new Map();
 
     this.platillosAGuardar.forEach((platillo, index) => {
-      // Obtener la cantidad del platillo utilizando el índice
       const cantidad = this.diccionarioDeCantidades[index] || 1;
-
-      // Obtener la descripción correspondiente utilizando el índice
       const descripcion = this.platillosDescripciones[index]?.descripcion || '';
 
       if (descripcion) {
-        // Si el platillo tiene descripción, lo agregamos directamente
-        const platilloConDescripcion: PlatilloPedido = {
+        platillosConDescripciones.push({
           id_platillo: platillo.id,
           precio_unitario: platillo.precio,
-          cantidad: cantidad,
+          cantidad,
           detalle: descripcion
-        };
-        platillosConDescripciones.push(platilloConDescripcion);
+        });
       } else {
-        // Si el platillo no tiene descripción, verificamos si ya existe en el Map para sumar la cantidad
         if (platillosSinDescripcion.has(platillo.id)) {
           let platilloExistente = platillosSinDescripcion.get(platillo.id)!;
           platilloExistente.cantidad += cantidad;
         } else {
-          // Si no existe, lo agregamos al Map
           platillosSinDescripcion.set(platillo.id, {
             id_platillo: platillo.id,
             precio_unitario: platillo.precio,
-            cantidad: cantidad,
+            cantidad,
             detalle: ''
           });
         }
       }
     });
 
-    // Convertimos los platillos sin descripción a un array y los combinamos con los de descripción
     const todosLosPlatillos = [...platillosConDescripciones, ...Array.from(platillosSinDescripcion.values())];
-
     const id_mesa = this.numeroMesa;
     const id_empleado = this.sessionService.getUsuario()?.id || '';
-
     const pedidoCompleto = {
       platillos: todosLosPlatillos,
       id_mesa: id_mesa,
       tipo: this.tipo.toLowerCase(),
       id_empleado: id_empleado
     };
-    console.log(pedidoCompleto)
 
+    if (this.rol_user === 2) {
+      this.mostrarModal = true; // Cambiar el estado de mostrarModal
+      console.log(this.mostrarModal);
+      this.registrarPedido(pedidoCompleto);
+    } else {
+      this.registrarPedido(pedidoCompleto);
+    }
+  }
+
+  registrarPedido(pedido: any) {
     const formData = new FormData();
-    formData.append('platillos', JSON.stringify(pedidoCompleto.platillos));
-    formData.append('id_mesa', id_mesa.toString());
-    formData.append('tipo', this.tipo.toLowerCase());
-    formData.append('id_empleado', id_empleado.toString());
+    formData.append('platillos', JSON.stringify(pedido.platillos));
+    formData.append('id_mesa', pedido.id_mesa.toString());
+    formData.append('tipo', pedido.tipo);
+    formData.append('id_empleado', pedido.id_empleado.toString());
     let id_restaurante = sessionStorage.getItem('id_restaurante');
     formData.append('id_restaurante', id_restaurante || '');
 
     this.pedidoselectService.storePedido(formData).subscribe(
       (response) => {
-        console.error('se registró el pedido', response);
-        this.toastr.success('Se registro el pedido correctamente','Éxito');
+        console.log('Se registró el pedido', response);
+        this.toastr.success('Se registró el pedido correctamente', 'Éxito');
       },
       (error) => {
         console.error('Error al almacenar el pedido', error);
-        this.toastr.error('El pedido no se registro','Error');
+        this.toastr.error('El pedido no se registró', 'Error');
       }
     );
-
     this.pedidoselectService.limpiarSeleccion();
     this.descripcionPedidoService.limpiarDescripciones();
     this.diccionarioDeCantidades = {};
@@ -242,8 +227,8 @@ export class RegistrarPedidoComponent implements OnInit {
     this.platillos = [];
     this.switchState = false;
     this.getPlatillos();
+    this.mostrarModal = false; // Cerrar el modal después de registrar el pedido
   }
-
 
   increment(index: number) {
     let cantidad = this.diccionarioDeCantidades[index] || 1;
@@ -260,5 +245,4 @@ export class RegistrarPedidoComponent implements OnInit {
   onImgError(event: any) {
     event.target.src = 'assets/image/Imagen-rota.jpg';
   }
-
 }
